5d656611f4e89e8afa0b00ecdcfc6b428de0f919
Henderson Hummel <hhummel@redhat.com>
2021-06-07 14:52:06 -0700

    Add tls-verify options to docker commands (for podman support)
    Because many of the installations of galaxy_ng we use for testing will
    use self-signed certs, if we want to use podman in our tests we need to
    make it possible to turn off tls-verify.
    
    These changes make that possible.
    
    In addition this commit simplifies the interface for the  `docker tag`
    argument - now rather than requiring an explicit `version` argument, the
    caller of the function should simply put the version in the tag that
    they applying to the image.

--- galaxykit/dockerutils.py
+++ galaxykit/dockerutils.py
@@ -13,8 +13,11 @@ class DockerClient:
 
     engine = ""
     registry = ""
+    tls_verify = True
 
-    def __init__(self, auth=None, engine="podman", registry="docker.io/library/"):
+    def __init__(
+        self, auth=None, engine="podman", registry="docker.io/library/", tls_verify=True
+    ):
         """
         auth should be `(username, password)` for logging into the docker registry.
 
@@ -22,33 +25,68 @@ class DockerClient:
         """
         self.engine = engine
         self.registry = registry
+        self.tls_verify = tls_verify
 
         if auth:  # we only need to auth if creds are supplied
-            run_args = [
-                engine,
-                "login",
-                registry,
-                "--username",
-                auth[0],
-                "--password",
-                auth[1],
-            ]
+            if engine == "podman":
+                run_args = [
+                    engine,
+                    "login",
+                    registry,
+                    "--username",
+                    auth[0],
+                    "--password",
+                    auth[1],
+                    f"--tls-verify={tls_verify}"  # this is only valid/necessary
+                    # with podman
+                ]
+            else:
+                run_args = [
+                    engine,
+                    "login",
+                    registry,
+                    "--username",
+                    auth[0],
+                    "--password",
+                    auth[1],
+                ]
             run(run_args)
 
     def pull_image(self, image_name):
         """
         pull an image from the configured default registry
         """
-        run([self.engine, "pull", self.registry + image_name])
+        if self.engine == "podman" and not self.tls_verify:
+            run([self.engine, "pull", self.registry + image_name, "--tls-verify=False"])
+        else:
+            run([self.engine, "pull", self.registry + image_name])
 
-    def tag_image(self, image_name, newtag, version="latest"):
+    def tag_image(self, image_name, newtag):
         """
-        Tags an image with the given tag
+        Tags an image with the given tag (prepends the registry to the tag.)
         """
-        run([self.engine, "image", "tag", image_name, f"{newtag}:{version}"])
+        run(
+            [
+                self.engine,
+                "image",
+                "tag",
+                image_name,
+                f"{self.registry}/{newtag}",
+            ]
+        )
 
     def push_image(self, image_tag):
         """
-        Pushes an image
+        Pushes an image to the registry
         """
-        run([self.engine, "push", image_tag])
+        if self.engine == "podman" and not self.tls_verify:
+            run(
+                [
+                    self.engine,
+                    "push",
+                    f"{self.registry}/{image_tag}",
+                    "--tls-verify=False",
+                ]
+            )
+        else:
+            run([self.engine, "push", f"{self.registry}/{image_tag}"])
